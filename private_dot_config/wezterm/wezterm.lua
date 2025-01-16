4f62d30467fc1d6568ed23cbe1545bdfea620c88
Ryan Mitchell <ryan@mitchell.plus>
2025-01-16 16:23:43 -0500

    Add custom WezTerm theme.

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,7 +2,53 @@ local wezterm = require 'wezterm'
 local act = wezterm.action
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Alabaster'
+-- config.color_scheme = 'Alabaster'
+config.colors = {
+  background = '#F7F7F7',
+  foreground = '#434343',
+
+  cursor_fg = '#F7F7F7',
+  cursor_bg = '#434343',
+  cursor_border = '#434343',
+  compose_cursor = 'orange',
+
+  selection_fg = 'black',
+  selection_bg = '#fffacd',
+  scrollbar_thumb = '#222222',
+  split = '#444444',
+
+  ansi = {
+    '#000000',
+    '#AA3731',
+    '#448C27',
+    '#CB9000',
+    '#325CC0',
+    '#7A3E9D',
+    '#0083B2',
+    '#BBBBBB',
+  },
+  brights = {
+    '#777777',
+    '#F05050',
+    '#60CB00',
+    '#FFBC5D',
+    '#007ACC',
+    '#E64CE6',
+    '#00AACB',
+    '#FFFFFF',
+  },
+
+  copy_mode_active_highlight_bg = { Color = '#000000' },
+  copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
+  copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
+  copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
+
+  quick_select_label_bg = { Color = 'peru' },
+  quick_select_label_fg = { Color = '#ffffff' },
+  quick_select_match_bg = { AnsiColor = 'Navy' },
+  quick_select_match_fg = { Color = '#ffffff' },
+}
+
 config.font_size = 11.0
 config.window_padding = { bottom = 0, left = 0, right = 0, top = 0  }
 
