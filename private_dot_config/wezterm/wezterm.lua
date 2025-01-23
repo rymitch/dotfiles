786847b0b1377a25ade110e232ec2f6e05c59a37
Ryan Mitchell <ryan@mitchell.plus>
2025-01-23 12:47:54 -0500

    Improve WezTerm URL mouse clicks.

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -139,6 +139,25 @@ for i = 1, 8 do
   })
 end
 
+
+config.mouse_bindings = {
+  { -- Disable the default click behavior
+    event = { Up = { streak = 1, button = "Left"} },
+    mods = "NONE",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  { -- Ctrl-click will open the link under the mouse cursor
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+  { -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+      event = { Down = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = wezterm.action.Nop,
+  },
+}
+
 config.switch_to_last_active_tab_when_closing_tab = true
 config.tab_max_width = 32
 config.tab_bar_at_bottom = true
