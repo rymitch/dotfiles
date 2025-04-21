8ce8c73ed719c1c05198a33dcaba8c0b29b56083
Ryan Mitchell <ryan.mitchell@beckman.com>
2025-04-21 11:59:19 -0400

    Add LazyVim configuration.

---
+++ private_dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt
+
+-- Disable all snacks animations, which are a little
+-- laggy. (Snacks provides the smooth scrolling effect.)
+vim.g.snacks_animate = false
+
+-- Disable relative numbering, which is a little
+-- laggy.
+opt.relativenumber = false
---
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  -- Add Modus
+  { "ishan9299/modus-theme-vim" },
+
+  -- Add Alabaster
+  { "p00f/alabaster.nvim" },
+
+  -- Add GitHub themes
+  { "projekt0n/github-nvim-theme" },
+
+  -- Set the default color scheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_light",
+    },
+  },
+}
