774113d365b5e2c06ccf592a359284b4695c8f95
Ryan Mitchell <ryan.mitchell@beckman.com>
2025-04-21 12:05:06 -0400

    Add LazyVim lock file. Add Modus theme.

---
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- Add Modus
+  { "ishan9299/modus-theme-vim" },
+
+  -- Set the default color scheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "modus-operandi",
+    },
+  },
+}
