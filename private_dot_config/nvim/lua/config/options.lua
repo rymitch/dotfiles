e1cc445413a60c070aebb541cf027e537a5cc39b
Ryan Mitchell <ryan.mitchell@beckman.com>
2025-04-23 11:10:21 -0400

    Disable laggy operations.

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local opt = vim.opt
+
+-- Disable all snacks animations, which are a little
+-- laggy. (Snacks provides the smooth scrolling effect.)
+vim.g.snacks_animate = false
+
+-- Disable relative numbering, which is a little
+-- laggy.
+opt.relativenumber = false
--- private_dot_config/nvim/lua/plugins/snacks.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "folke/snacks.nvim",
-  opts = {
-    -- disable "smooth" scrolling
-    scroll = { enabled = false },
-  },
-}
