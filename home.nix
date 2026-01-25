3d7e612ce55b8514ec58a0578b4e1f186d7c0634
Ryan Mitchell <ryan@mitchell.plus>
2026-01-25 14:28:39 +0000

    Add cowsay.

--- home.nix
+++ home.nix
@@ -3,6 +3,9 @@
 {
   home.username = "rmitchell";
   home.homeDirectory = "/home/rmitchell";
+  home.packages = with pkgs; [
+    cowsay
+  ];
   programs.git.enable = true;
   home.stateVersion = "25.11";
 }
